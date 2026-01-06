0794f209db6b161408ea6f7a3556344c9464767d
Ram ji <mishraramji039@gmail.com>
2026-01-06 10:13:09 +0530

    improve design

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -3,7 +3,7 @@ import { projects } from "../projects/Projects";
 import { useNavigate } from "react-router-dom";
 
 export default function ProjectSection() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <section id="projects" className="max-w-6xl mx-auto px-6 py-10">
       <h2 className="text-3xl font-bold">Featured Projects</h2>
@@ -18,8 +18,13 @@ export default function ProjectSection() {
         ))}
       </div>
 
-      <div onClick={()=>navigate("work")} className="text-center mt-15 hover:scale-[1.02] hover:text-neutral-800">
-        <button className="text-lg">Show More Projects</button>
+      <div className=" text-center mt-15 hover:scale-[1.02] hover:text-neutral-800">
+        <button
+          onClick={() => navigate("work")}
+          className="text-lg px-3 py-1 bg-white rounded-md border border-slate-300 hover:bg-slate-50"
+        >
+          Show More Projects
+        </button>
       </div>
     </section>
   );
